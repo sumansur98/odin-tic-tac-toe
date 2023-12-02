3dd1c9c7375643c02ead1e1d50f322f3dfc36f91
Suman Sur <sumansur.cs98@gmail.com>
2023-12-02 17:47:38 +0530

    styling complete, tie condition left

--- script.js
+++ script.js
@@ -19,12 +19,17 @@ function GameBoard(){
     let cols = 3;
     let board = [];
 
-    for(let i=0;i<rows;i++){
-        board[i] = []
-        for(let j=0;j<cols;j++){
-            board[i].push(Cell());
+    const resetBoard = ()=>{
+        for(let i=0;i<rows;i++){
+            board[i] = []
+            for(let j=0;j<cols;j++){
+                board[i].push(Cell());
+            }
         }
     }
+
+    resetBoard();
+    
 
     const getBoard = () => board;
 
@@ -49,45 +54,45 @@ function GameBoard(){
         if(board[0][0].getCellValue()!==0 
         && (board[0][0].getCellValue()===board[0][1].getCellValue()) 
         && (board[0][0].getCellValue()===board[0][2].getCellValue())){
-            return board[0][0].getCellValue();
+            return true;
         }else if(board[1][0].getCellValue()!==0 
         && (board[1][0].getCellValue()===board[1][1].getCellValue()) 
         && (board[1][0].getCellValue()===board[1][2].getCellValue())){
-            return board[1][0].getCellValue();
+            return true
         }else if(board[2][0].getCellValue()!==0 
         && (board[2][0].getCellValue()===board[2][1].getCellValue()) 
         && (board[2][0].getCellValue()===board[2][2].getCellValue())){
-            return board[2][0].getCellValue();
+            return true
         }else if(board[0][0].getCellValue()!==0 
         && (board[0][0].getCellValue()===board[1][0].getCellValue()) 
         && (board[1][0].getCellValue()===board[2][0].getCellValue())){
-            return board[0][0].getCellValue();
+            return true
         }else if(board[0][0].getCellValue()!==0 
         && (board[0][0].getCellValue()===board[1][0].getCellValue()) 
         && (board[1][0].getCellValue()===board[2][0].getCellValue())){
-            return board[0][0].getCellValue();
+            return true
         }else if(board[0][1].getCellValue()!==0 
         && (board[0][1].getCellValue()===board[1][1].getCellValue()) 
         && (board[1][1].getCellValue()===board[2][1].getCellValue())){
-            return board[0][1].getCellValue();
+            return true
         }else if(board[0][2].getCellValue()!==0 
         && (board[0][2].getCellValue()===board[1][2].getCellValue()) 
         && (board[1][2].getCellValue()===board[2][2].getCellValue())){
-            return board[0][2].getCellValue();
+            return true
         }else if(board[0][0].getCellValue()!==0 
         && (board[0][0].getCellValue()===board[1][1].getCellValue()) 
         && (board[1][1].getCellValue()===board[2][2].getCellValue())){
-            return board[0][0].getCellValue();
+            return true
         }else if(board[0][2].getCellValue()!==0 
         && (board[0][2].getCellValue()===board[1][1].getCellValue()) 
         && (board[1][1].getCellValue()===board[2][0].getCellValue())){
-            return board[0][0].getCellValue();
+            return true
         }else{
             return false;
         }
     }
 
-    return {getBoard, printBoard, markCell, checkWin};
+    return {getBoard, printBoard, markCell, checkWin, resetBoard};
 
 }
 
@@ -115,22 +120,33 @@ function GameController(){
     const playRound = (row, col)=>{
         board.markCell(activePlayer.token, row, col);
         board.printBoard();
+
         if(board.checkWin()){
-            console.log(`${activePlayer.player} won`);
-            return;
+            console.log('won');
+            //board.resetBoard();
+            return board.checkWin();
         }
-        switchActivePlayer();
+
+       switchActivePlayer();
     }
 
+
+    const resetGame = ()=>{
+        board.resetBoard();
+        activePlayer = players[0];
+    }
+
+
     return {
-        switchActivePlayer, playRound, getActivePlayer, getBoard : board.getBoard()
+        switchActivePlayer, playRound, getActivePlayer, getBoard : board.getBoard(), resetGame
     }
 }
 
 function ScreenController(){
     const game = GameController();
     const boardDiv = document.querySelector('.board')
-    const turnDiv = document.querySelector('.turn')
+    const turnDiv = document.querySelector('.turn');
+    let winFlag = false;
 
     const updateScreen = () => {
         boardDiv.textContent = '';
@@ -143,20 +159,45 @@ function ScreenController(){
                 cell.classList.add('cell');
                 cell.dataset.row = i;
                 cell.dataset.column = j;
-                cell.textContent = cellEle.getCellValue();
+                cellVal = cellEle.getCellValue();
+                if(cellVal === 0){
+                    cell.classList.add('no-value');
+                    cell.textContent = '';
+                }else if(cellVal === 1){
+                    cell.classList.add('x-value');
+                    cell.textContent = 'X';
+                }else{
+                    cell.classList.add('o-value');
+                    cell.textContent = 'O';
+                }
                 boardDiv.appendChild(cell);
             })
         });
+
+        if(winFlag){
+            turnDiv.textContent = `${activePlayer.player} won`;
+            
+        }
+
     }
 
     const clickHandler = (e) => {
         let row = e.target.dataset.row;
         let col = e.target.dataset.column;
-        game.playRound(row, col);
+        winFlag = game.playRound(row, col);
         updateScreen();
     }
 
     boardDiv.addEventListener('click', clickHandler);
+
+    const resetGameClicked = ()=>{
+        game.resetGame();
+        winFlag = false;
+        updateScreen();
+
+    }
+
+    document.querySelector('#btnReset').addEventListener('click', resetGameClicked);
 
     updateScreen();
 
